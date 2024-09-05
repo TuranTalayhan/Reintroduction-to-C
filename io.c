e002d9ce7d48bc11fead2c4c3f1e2d60fc25b69e
Marcus <s195080@dtu.dk>
2024-09-05 11:40:42 +0200

    changed the read char function so it no longer gives an error

--- io.c
+++ io.c
@@ -6,14 +6,18 @@
 #include "io.h"
 
 /* Reads next char from stdin. If no more characters, it returns EOF */
-int
-read_char() {
+int read_char() {
     char a;
     long bytes_read;
     do {
         bytes_read = read(0, &a, 1);
-    } while(bytes_read == -1 && errno == EINTR);
-  return EOF;
+    } while (bytes_read == -1 && errno == EINTR);
+
+    if (bytes_read == 1) {
+        return (int)a;
+    } else {
+        return EOF;
+    }
 }
 
 /* Writes c to stdout.  If no errors occur, it returns 0, otherwise EOF */
@@ -27,12 +31,9 @@ write_char(char c) {
 }
 
 /* Writes a null-terminated string to stdout.  If no errors occur, it returns 0, otherwise EOF */
-int
-write_string(char* s) {
-    if(write(1,s, strlen(s))!=-1){
-        return 0;
-    }
-    return EOF;
+int write_string(char* s) {
+    ssize_t result = write(1, s, strlen(s));
+    return (result != -1) ? 0 : EOF;
 }
 
 /* Writes n to stdout (without any formatting).   
