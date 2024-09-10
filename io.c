564fa81fdc54436eb42650e1e3eeaddc8dfd6771
TuranTalayhan <tura0015@gmail.com>
2024-09-10 10:36:32 +0200

    reformatted code

--- io.c
+++ io.c
@@ -6,7 +6,7 @@
 int read_char() {
     char a;
     if (read(0, &a, 1) == 1) {
-        return (int)a;
+        return (int) a;
     } else {
         return EOF;
     }
@@ -14,14 +14,14 @@ int read_char() {
 
 /* Writes c to stdout.  If no errors occur, it returns 0, otherwise EOF */
 int write_char(char c) {
-    if(write(1, &c, 1)!=-1){
+    if (write(1, &c, 1) != -1) {
         return 0;
     }
     return EOF;
 }
 
 /* Writes a null-terminated string to stdout.  If no errors occur, it returns 0, otherwise EOF */
-int write_string(char* s) {
+int write_string(char *s) {
     ssize_t result = write(1, s, strlen(s));
     return (result != -1) ? 0 : EOF;
 }
