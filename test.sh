8105c1c8248ee545e2af4871f3e981b9c1a4e258
Marcus <s195080@dtu.dk>
2024-09-16 14:00:16 +0200

    added 3 more tests

--- test.sh
+++ test.sh
@@ -8,3 +8,14 @@ out="0,3,5;"
 [[ $(./cmd_int <<< "$in") == "$out"* ]] && echo "PASSED" || echo "FAILED"
 
 
+in="abccbaabcq"
+out="5;"
+[[ $(./cmd_int <<< "$in") == "$out"* ]] && echo "PASSED" || echo "FAILED"
+
+in="aaaaaq"
+out="0,1,2,3,4;"
+[[ $(./cmd_int <<< "$in") == "$out"* ]] && echo "PASSED" || echo "FAILED"
+
+in="bbbbbbbbbaq"
+out="9;"
+[[ $(./cmd_int <<< "$in") == "$out"* ]] && echo "PASSED" || echo "FAILED"
